4b7a0a211939889b0adc7f637dd0605b45578157
yishikawa-jss <yuya_ishikawa@jrass.jp>
2022-05-07 20:52:38 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yuya/ishikawa/taskapp/Task.kt
@@ -0,0 +1,18 @@
+package jp.techacademy.yuya.ishikawa.taskapp
+
+import java.io.Serializable
+import java.util.Date
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class Task : RealmObject(), Serializable {
+    var title: String = ""      // タイトル
+    var contents: String = ""   // 内容
+    var date: Date = Date()     // 日時
+
+    var category: String = ""
+
+    // idをプライマリーキーとして設定
+    @PrimaryKey
+    var id: Int = 0
+}
